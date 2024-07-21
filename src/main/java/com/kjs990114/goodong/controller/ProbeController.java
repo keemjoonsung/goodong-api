7d4d85f730313ac0affbf38223dd4a2d31daff0a
keemjoonsung <kjs990114@naver.com>
2024-07-21 15:32:42 +0900

    ci cd test

---
+++ src/main/java/com/kjs990114/goodong/controller/ProbeController.java
@@ -0,0 +1,14 @@
+package com.kjs990114.goodong.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProbeController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> probe(){
+        return ResponseEntity.ok("ok");
+    }
+}
