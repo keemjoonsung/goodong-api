07178ad964c2aa8cea3f21d967e1730d2ef52377
keemjoonsung <kjs990114@naver.com>
2024-09-06 21:46:13 +0900

    ai auto generation tags API

---
+++ src/main/java/com/kjs990114/goodong/presentation/endpoint/AiEndPoint.java
@@ -0,0 +1,33 @@
+package com.kjs990114.goodong.presentation.endpoint;
+
+import com.kjs990114.goodong.presentation.common.CommonResponseEntity;
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ai")
+public class AiEndPoint {
+
+    @PostMapping("/description")
+    public CommonResponseEntity<AiResponse> aiService(
+            @RequestBody ImageRequest image
+    ) {
+        return new CommonResponseEntity<>(new AiResponse(List.of("태그 1", "태그 2", "태그 3")));
+    }
+
+    public static class ImageRequest {
+
+    }
+    public static class AiResponse {
+        private List<String> tags;
+        public AiResponse(List<String> tags) {}
+    }
+
+
+}
